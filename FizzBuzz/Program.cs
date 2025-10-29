8cba711dc8988f3172a2c4e22188865f2e5d42e7
Jakub <uT_3773@zsp3zamosc.pl>
2025-10-29 11:09:51 +0100

    Dodaj pliki projektów.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj liczbę: ");
+            string input = Console.ReadLine();
+
+            if (double.TryParse(input, out double number))
+            {
+                Console.WriteLine(FizzBuzz(number));
+            }
+            else
+            {
+                Console.WriteLine("NaN");
+            }
+        }
+
+        static string FizzBuzz(double input)
+        {
+            if (input % 3 == 0 && input % 5 == 0)
+                return "FizzBuzz";
+
+            if (input % 3 == 0)
+                return "Fizz";
+
+            if (input % 5 == 0)
+                return "Buzz";
+
+            return input.ToString();
+        }
+
+    }
+}
